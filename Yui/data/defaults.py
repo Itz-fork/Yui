3e4da5da3f50a446b2526a8fa0604e59e4bd8828
Itz-fork <git.itzfork@gmail.com>
2022-06-17 19:57:04 +0530

    Fixed KeyError

--- Yui/data/defaults.py
+++ Yui/data/defaults.py
@@ -2,6 +2,7 @@
 import os
 from config import Config
 
+
 class Defaults():
     """
     All the default data of Yui Chat bot
@@ -13,13 +14,14 @@ class Defaults():
         CHAT_LOG_DB - Dict to save users chat logs (temp)
         Engines_list - List of engines names available in OpenAI 
     """
-    Chat_Log = """
+    Chat_Log = f"""
 You: Hey, Wassup?
-{bname}: Hey there!
-You: I'm {uname}!
-{bname}:
-""".format(bname=Config.CHAT_BOT_NAME)
+{Config.CHAT_BOT_NAME}: Hey there!
+You: I'm""" + " {uname} " + f"""!
+{Config.CHAT_BOT_NAME}:
+"""
     Engine = "text-davinci-002"
     Max_Tokens = int(os.environ.get("MAX_TOKENS", 100))
     CHAT_LOG_DB = {}
-    Engines_list = ["davinci", "curie", "babbage", "ada", "text-davinci-002", "text-curie-001", "text-babbage-001", "text-ada-001",]
+    Engines_list = ["davinci", "curie", "babbage", "ada", "text-davinci-002",
+                    "text-curie-001", "text-babbage-001", "text-ada-001", ]
